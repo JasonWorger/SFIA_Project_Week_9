35c6c54f67c25217c997d8026cd66b71cea9e01c
Jason Worger <jasonworger@gmail.com>
2020-08-21 11:14:41 +0100

    adding route

--- service2/application/routes.py
+++ service2/application/routes.py
@@ -4,9 +4,17 @@ from application import app
 import random
 
 
-#generates a random football team name
-@app.route('/get_team',methods=['GET'])
+#generates a random 2 worded football team name
+@app.route('/get_team_1',methods=['GET'])
 def get_team():
-    teams = ['The Glorious Bottle Rockets','The Grim Koalas','Heavenly Llamas','The Coyotes','The Hot Cardinals','Raiders','The Goats','The Vipers','Ruthless Ducks','The Parrots']
+    teams = ['The Devils','The Koalas','Heavenly Llamas','The Coyotes','The Cardinals','The Raiders','The Goats','The Vipers','Ruthless Ducks','The Parrots']
+    team = teams[random.randrange(10)]
+    return Response(team,mimetype='text/plain')
+
+
+#generates a random 3 worded football team name
+@app.route('/get_team_2',methods=['GET'])
+def get_team():
+    teams = ['The Bottle Rockets','The Old Mavericks','The Careless Roaches','The Old Bois','The Slow Chargers','The Stark Squids','The Ancient Dogs','The Awesome Porcupines','The Determined Beasts','The Lucky Stingers']
     team = teams[random.randrange(10)]
     return Response(team,mimetype='text/plain')

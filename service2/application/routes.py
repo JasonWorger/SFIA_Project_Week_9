9daf23fc8cb5251298ac669a8b32e08e0ffe8c57
Jason Worger <jasonworger@gmail.com>
2020-08-21 18:12:26 +0100

    fixing service 2 routes

--- service2/application/routes.py
+++ service2/application/routes.py
@@ -4,17 +4,9 @@ from application import app
 import random
 
 
-#generates a random 2 worded football team name
-@app.route('/get_team_1',methods=['GET'])
-def get_team():
-    teams = ['The Devils','The Koalas','Heavenly Llamas','The Coyotes','The Cardinals','The Raiders','The Goats','The Vipers','Ruthless Ducks','The Parrots']
-    team = teams[random.randrange(10)]
-    return Response(team,mimetype='text/plain')
-
-
-#generates a random 3 worded football team name
-@app.route('/get_team_2',methods=['GET'])
-def get_team():
-    teams = ['The Bottle Rockets','The Old Mavericks','The Careless Roaches','The Old Bois','The Slow Chargers','The Stark Squids','The Ancient Dogs','The Awesome Porcupines','The Determined Beasts','The Lucky Stingers']
-    team = teams[random.randrange(10)]
-    return Response(team,mimetype='text/plain')
+#generates a team name
+@app.route('/get/city',methods=['GET'])
+def get_city():
+    cities = ['London','Manchester','Leeds']
+    city = cities[random.randrange(3)]
+    return Response(city,mimetype='text/plain')

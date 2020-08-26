f5e3a10b138c9605bca6edfe1dd1b2826fb8c235
JasonWorger <jasonworger@gmail.com>
2020-08-26 20:55:51 +0000

    adding tests

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -12,7 +12,11 @@ def home():
 
 
 
-@app.route('/generate', methods=['GET','POST'])
+@app.route('/generateView', methods=['GET'])
+def generate_view():
+    return render_template('generate.html', title= 'Team Name')
+
+@app.route('/generate', methods=['GET'])
 def generate_team():
     city = requests.get('http://service2:5001/get/city')
     name = requests.get('http://service3:5002/get/name')
@@ -23,4 +27,5 @@ def generate_team():
     db.session.commit()
     teams_record=db_data.query.all()
 
-    return render_template('generate.html', title= 'Team Name', city=city.text, name=name.text, slogan=slogan.text, teams=teams_record)
+    return render_template('generate.html', title= 'Team Name')
+

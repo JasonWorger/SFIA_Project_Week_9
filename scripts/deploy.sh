af5afb63fa4b371168a2325621e6b73ced272e75
JasonWorger <jasonworger@gmail.com>
2020-08-28 14:51:38 +0000

    adding sudo to build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,8 @@ ssh manager << EOF
 cd SFIA_Project_Week_9/
 export SECRET_KEY=${SECRET_KEY} 
 export DATABASE_URI=${DATABASE_URI} 
+pwd
+whoami
+# scp /home/jason/SFIA_Project_Week_9 username@a:/path/to/destination
 docker stack deploy --compose-file docker-compose.yaml sfia2
 EOF

872f32e16d14f871b2488b34032e672adf0c6427
JasonWorger <jasonworger@gmail.com>
2020-08-28 13:41:50 +0000

    changing inventory file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo docker-compose down --rmi all
-sudo docker-compose build
-sudo docker login
-sudo docker-compose push
+sudo chmod 666 /var/run/docker.sock
+
+docker-compose down --rmi all
+docker-compose build
+docker login
+docker-compose push
